a5500a2395455a3865f996490c78c32f88bbbb4e
sp <56139772+sikhapentyala@users.noreply.github.com>
2020-07-19 20:05:35 -0700

    Turn restrictions

--- OSM-Connecitvity-Backend-Revised/JunctionNode.cs
+++ OSM-Connecitvity-Backend-Revised/JunctionNode.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
@@ -15,6 +15,15 @@ namespace OSM_Connecitvity_Backend_Revised
 
         public List<string> roadTypes { get; set; }
         public int label = 0;
+
+        //Added in SUM
+        //relationid,role signifying which (from,to,via) are we at while exploring the node
+        //This will work for only no retsrictions 
+        //{key:rlnid, Value = [counter,role]}
+        //COUNTER is not required as we do not use it, need to modify to Dictionary<string,string> - {key:rlnid, Value = role}
+        //public Dictionary<string, List<string>> restrictions { get; set; }
+        public Dictionary<string, string> restrictions { get; set; }
+
         public JunctionNode(string id, Dictionary<string,string> wayToNodeMap, List<string> roadTypes, float Lat, float Lng)
         {
             this.Id = id;
@@ -22,12 +31,14 @@ namespace OSM_Connecitvity_Backend_Revised
             this.roadTypes = roadTypes;
             this.Lat = Lat;
             this.Lng = Lng;
-    }
+            this.restrictions = new Dictionary<string, string>();
+        }
 
         public JunctionNode()
         {
             this.roadTypes = new List<string>();
             this.wayToNodeMap = new Dictionary<string, string>();
+            this.restrictions = new Dictionary<string, string>();
         }
     }
 }
